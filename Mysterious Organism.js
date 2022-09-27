1d1424a50fee6fa3d61485783ee25d19547db73f
AliWilkinson <96079887+AliWilkinson@users.noreply.github.com>
2022-09-27 12:57:00 +0100

    Function to mutate a random base

--- Mysterious Organism.js
+++ Mysterious Organism.js
@@ -1,9 +1,29 @@
+let testArr = [
+  "G",
+  "G",
+  "C",
+  "G",
+  "T",
+  "T",
+  "A",
+  "A",
+  "C",
+  "A",
+  "G",
+  "C",
+  "C",
+  "G",
+  "T",
+];
+
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
+console.log(returnRandBase());
+console.log(returnRandBase());
 // Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
@@ -12,3 +32,37 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+
+//console.log(mockUpStrand());
+
+const pAequorFactory = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+    mutate() {
+      console.log(this.dna);
+      let index = Math.floor(Math.random() * 15);
+      let randomBase = this.dna[index];
+      let newBase = "";
+      if (randomBase === "A") {
+        const otherBases = ["T", "C", "G"];
+        newBase = otherBases[Math.floor(Math.random() * 3)];
+        return newBase;
+      } else if (randomBase === "T") {
+        const otherBases = ["A", "C", "G"];
+        newBase = otherBases[Math.floor(Math.random() * 3)];
+      } else if (randomBase === "C") {
+        const otherBases = ["A", "T", "G"];
+        newBase = otherBases[Math.floor(Math.random() * 3)];
+      } else if (randomBase === "G") {
+        const otherBases = ["A", "T", "C"];
+        newBase = otherBases[Math.floor(Math.random() * 3)];
+      }
+      this.dna[index] = newBase;
+      return this.dna;
+    },
+  };
+};
+
+const specemin1 = pAequorFactory(1, testArr);
+console.log(specemin1.mutate());
